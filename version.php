aff6b4a068b85ffdc305c6753a6a4415ae0071f2
Petr Skoda <petr@mutms.org>
2025-05-20 07:36:31 +0100

    Fix license

--- version.php
+++ version.php
@@ -20,7 +20,9 @@
  * Certifications plugin version.
  *
  * @package     tool_mucertify
+ * @copyright   2023 Open LMS (https://www.openlms.net/)
  * @copyright   2025 Petr Skoda
+ * @author      Petr Skoda
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
