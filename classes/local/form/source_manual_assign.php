41c61146757314d130c7522f499c343de190f641
Petr Skoda <petr@mutms.org>
2025-11-22 19:29:57 +0100

    Allow setting expiration on assignment

--- classes/local/form/source_manual_assign.php
+++ classes/local/form/source_manual_assign.php
@@ -37,6 +37,8 @@ final class source_manual_assign extends \tool_mulib\local\ajax_form {
     protected $dueoptional = true;
     /** @var \tool_mucertify\customfield\assignment_handler */
     protected $handler;
+    /** @var \stdClass */
+    protected $settings;
 
     #[\Override]
     protected function definition() {
@@ -46,6 +48,7 @@ final class source_manual_assign extends \tool_mulib\local\ajax_form {
         $context = $this->_customdata['context'];
 
         $settings = \tool_mucertify\local\certification::get_periods_settings($certification);
+        $this->settings = $settings;
 
         $this->arguments = ['certificationid' => $certification->id];
         source_manual_assign_users::add_element(
@@ -86,6 +89,15 @@ final class source_manual_assign extends \tool_mulib\local\ajax_form {
         }
         if ($settings->due1 !== null) {
             $mform->setDefault('timewindowdue', $now + $settings->due1);
+        }
+
+        if ($settings->recertify) {
+            $mform->addElement(
+                'date_time_selector',
+                'timeuntil',
+                get_string('untildate', 'tool_mucertify'),
+                ['optional' => true]
+            );
         }
 
         $mform->addElement('hidden', 'certificationid');
@@ -149,6 +161,20 @@ final class source_manual_assign extends \tool_mulib\local\ajax_form {
                     $errors['users'] = $error;
                     break;
                 }
+            }
+        }
+
+        if (!$data['users'] && !$data['cohortid']) {
+            $errors['users'] = get_string('required');
+            $errors['cohortid'] = get_string('required');
+        }
+
+        if ($this->settings->recertify && !empty($data['timeuntil'])) {
+            if (
+                $data['timeuntil'] - $this->settings->recertify <= $data['timewindowstart']
+                || $data['timeuntil'] - $this->settings->recertify <= time()
+            ) {
+                $errors['timeuntil'] = get_string('error');
             }
         }
 
