5810f062e49b75240e187b4e524a72a2b7a745e3
Petr Skoda <petr@mutms.org>
2025-12-23 11:06:48 +0100

    Fix category autocomplete

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mucertify';
-$plugin->version = 2025122150;
+$plugin->version = 2025122150.01;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [500, 501];
@@ -38,7 +38,7 @@ $plugin->supported = [500, 501];
 $plugin->release = 'mu-5.0.4-02+';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2025122150,
-    'tool_muprog' => 2025122150,
+    'tool_mulib' => 2025122150.01,
+    'tool_muprog' => 2025122150.01,
     'block_mucertify_my' => 2025122150,
 ];
